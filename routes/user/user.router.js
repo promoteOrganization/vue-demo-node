87d9dec1b70eaa42f96abfe1453de63671bad9a4
JiaXingBingGan <729869585@qq.com>
2017-06-12 15:22:30 +0800

    新增model公共操作方法

--- routes/user/user.router.js
+++ routes/user/user.router.js
@@ -3,68 +3,93 @@
  */
 var express = require('express');
 var router = express.Router();
-var User = require('../../model/user');
+var userModel = require('../../model/user'); // 引入user的model
+var RestMsg = require('../../middlewares/RestMsg');
+var Page = require('../../middlewares/page');
+var modelGenerator = require('../../model/common/modelGenerator'); // 引入model公共方法对象
+var User = modelGenerator(userModel, '_id');
 
 router.route('/')
     .get(function (req, res, next) {
         var query = {};
 
+        var restmsg = new RestMsg(); // 初始化restmsg，用于api返回信息
         User.find(query, function (err, objs) {
-                if (err) {
-                res.send(console.error(err));
+            if (err) {
+                restmsg.errorMsg(err);
+                res.send(restmsg);
             }
-            res.send(console.log(objs));
+            restmsg.successMsg(); // restmsg状态码设置为成功状态
+            restmsg.setResult(objs); // restmsg结果部分设置为封装好的objs
+            res.send(restmsg); // api返回restmsg
         })
     })
     .post(function (req, res, next) {
+        var restmsg = new RestMsg();
         var email = req.body.email;
         var password = req.body.pass;
-        var newUser = new User({
+        var newUser = {
             'email' : email,
             'password' : password
-        })
-        newUser.save(function (err, obj) {
+        }
+
+        User.save(newUser, function (err, obj) {
             if (err) {
-                res.send(console.error(err));
+                restmsg.errorMsg(err);
+                res.send(restmsg);
             }
-           res.send(console.log(obj));
+            restmsg.successMsg();
+            restmsg.setResult(obj);
+            res.send(restmsg);
         })
     })
 
 router.route('/:id')
     .get(function (req, res, next) {
+        var restmsg = new RestMsg();
         var userid = req.params.id;
 
-        User.findById({_id: userid}, function (err, obj) {
+        User.findById(userid, function (err, obj) {
             if (err) {
-                res.send(console.error(err));
+                restmsg.errorMsg(err);
+                res.send(restmsg);
             }
-            res.send(console.log(obj));
+            restmsg.successMsg();
+            restmsg.setResult(obj);
+            res.send(restmsg);
         })
     })
     .put(function (req, res, next) {
+        var restmsg = new RestMsg();
         var userid = req.params.id;
         var updateEmail = req.body.email;
 
-        var updateUser = new User({
+        var updateUser = {
             'email': updateEmail
-        })
+        }
 
-        updateUser.update({_id: userid}, function (err, obj) {
+        User.update({_id: userid}, updateUser, function (err, obj) {
             if (err) {
-                res.send(console.error(err));
+                restmsg.errorMsg(err);
+                res.send(restmsg);
             }
-            res.send(console.log(obj));
+            restmsg.successMsg();
+            restmsg.setResult(obj);
+            res.send(restmsg);
         })
     })
     .delete(function (req, res, next) {
-        var userid = req.param('id');
+        var restmsg = new RestMsg();
+        var userid = req.params.id;
 
-        User.remove({_id: userid}, function (err, obj) {
+        User.delete({_id: userid}, function (err, obj) {
             if (err) {
-                res.send(console.error(err));
+                restmsg.errorMsg(err);
+                res.send(restmsg);
             }
-            res.send(console.log(obj));
+            restmsg.successMsg();
+            restmsg.setResult(obj);
+            res.send(restmsg);
         })
     })
 
